db265f2c5bef7462d9b21f6d32deb40bafda8652
Lee <dpemdnjem23@gmail.com>
2025-05-01 17:23:25 +0900

    다시

---
+++ src/pages/Mypage/profilepost.tsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import { Input } from "@/components/ui/form";
+import { Camera } from "lucide-react";
+
+const ProfilePost = () => {
+  return (
+      <Form onSubmit={(value) => {}}>
+        {({ register, formState }) => (
+            return(
+                <>
+          <div>
+            <div className=" w-[170px] h-[170px] bg-[#D9D9D9]"></div>
+            <Camera className="text-[black]"></Camera>
+          </div>
+
+
+<Input></Input>
+</>
+            )
+        )}
+      </Form>
+  );
+};
+
+export default ProfilePost;
