7b5b56e2c380dc62dfeaa7f507b87220ea026d22
lys118 <dldudtn118a@gmail.com>
2025-05-23 10:17:50 +0900

    매칭이미지 추가

--- src/pages/interview/mathchingregister.tsx
+++ src/pages/interview/mathchingregister.tsx
@@ -72,65 +72,77 @@ const InterviewRegister = () => {
             />
             {/* 인원수 입력되면 자기소개 */}
             {matchType && (
-              <>
-                <ContentsBox title="자신을 소개해주세요">
+              <div className="mt-[50px]">
+                <ContentsBox title="면접에서 말할 자기소개를 간단히 작성해주세요.">
                   <Controller
                     control={control}
                     name="introduce"
                     render={({ field }) => (
                       <Textarea
-                        placeholder="ex) 데이터보다 사람 마음을 읽는 마케터를 꿈꿔요"
+                        placeholder="ex) 문제를 구조적으로 정리하고 해결하는 걸 좋아하는 UX 디자이너 지망생입니다."
                         {...field}
                       />
                     )}
                   />
                 </ContentsBox>
-              </>
+              </div>
             )}
 
             {/* 자기소개 입력되면 면접유형 */}
             {introduce && introduce.trim().length > 0 && (
-              <ContentsBox title="어떤 종류의 면접을 준비하시나요?">
-                <Controller
-                  control={form.control}
-                  name="interviewType"
-                  render={({ field }) => (
-                    <div className="w-fll flex flex-row gap-[20px]">
-                      <Button
-                        type="button"
-                        size="register"
-                        className="text-black text-[18px]"
-                        variant={
-                          field.value === "PERSONALITY" ? "outline" : "register"
-                        }
-                        onClick={() => field.onChange("PERSONALITY")}
-                      >
-                        인성
-                      </Button>
-                      <Button
-                        type="button"
-                        size="register"
-                        className="text-black text-[18px]"
-                        variant={field.value === "JOB" ? "outline" : "register"}
-                        onClick={() => field.onChange("JOB")}
-                      >
-                        직무
-                      </Button>
-                      <Button
-                        type="button"
-                        size="register"
-                        className="text-black text-[18px]"
-                        variant={
-                          field.value === "TECHNICAL" ? "outline" : "register"
-                        }
-                        onClick={() => field.onChange("TECHNICAL")}
-                      >
-                        기술
-                      </Button>
-                    </div>
-                  )}
-                />
-              </ContentsBox>
+              <div className="relative mt-[50px]">
+                <ContentsBox title="어떤 종류의 면접을 준비하시나요?">
+                  <p className="absolute top-1 left-65 text-gray-400">
+                    *선택한 유형은 매칭 참고용으로만 사용돼요.
+                  </p>
+                  <Controller
+                    control={form.control}
+                    name="interviewType"
+                    render={({ field }) => (
+                      <div className="w-fll flex flex-row gap-[20px]">
+                        <Button
+                          type="button"
+                          size="register"
+                          className="text-black text-[18px]"
+                          variant={
+                            field.value === "PERSONALITY"
+                              ? "outline"
+                              : "register"
+                          }
+                          onClick={() => field.onChange("PERSONALITY")}
+                        >
+                          <img src="/matchingIcon/인성.svg" alt="1:1" />
+                          인성
+                        </Button>
+                        <Button
+                          type="button"
+                          size="register"
+                          className="text-black text-[18px]"
+                          variant={
+                            field.value === "JOB" ? "outline" : "register"
+                          }
+                          onClick={() => field.onChange("JOB")}
+                        >
+                          <img src="/matchingIcon/직무.svg" alt="1:1" />
+                          직무
+                        </Button>
+                        <Button
+                          type="button"
+                          size="register"
+                          className="text-black text-[18px]"
+                          variant={
+                            field.value === "TECHNICAL" ? "outline" : "register"
+                          }
+                          onClick={() => field.onChange("TECHNICAL")}
+                        >
+                          <img src="/matchingIcon/기술.svg" alt="1:1" />
+                          기술
+                        </Button>
+                      </div>
+                    )}
+                  />
+                </ContentsBox>
+              </div>
             )}
             {/* 인터뷰타입 + 자기소개 입력되면 면접유형 */}
             {interviewType && introduce && introduce.trim().length > 0 && (
