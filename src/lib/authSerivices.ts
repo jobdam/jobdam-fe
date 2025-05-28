ab868ef60274529ec100535f4cdf696785482d89
lys118 <dldudtn118a@gmail.com>
2025-05-28 19:48:45 +0900

    refresh만료시 토큰지우기추가

--- src/lib/authSerivices.ts
+++ src/lib/authSerivices.ts
@@ -30,7 +30,7 @@ export const refreshAccessToken = async (): Promise<string | void> => {
     return token;
   } catch {
     //refreshToken이 만료된경우라면 로그아웃 처리한다.
-    useLogout();
+    //useLogout(); 훅이라서 컴포넌트에서사용필요!
 
     throw new Error("Refresh token has expired or is invalid");
   }
